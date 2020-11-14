f3a36e802c4f92787aa83da6444b501e3893a3d3
JustTapia <38672129+JustTapia@users.noreply.github.com>
2020-11-13 22:03:08 -0600

    Añadido prueba mutex

--- prueba1.c
+++ prueba1.c
@@ -4,28 +4,71 @@
 #include <string.h>
 #include <stdio.h> 
 #include <unistd.h>
+#include <pthread.h> 
 
+typedef struct shm_content
+{
+      pthread_mutex_t   mutex;
+}shm_content;
 
+pthread_mutex_t    *mptr; //Mutex Pointer
+pthread_mutexattr_t matr; //Mutex Attribute
+
+int   shared_mem_id;      //shared memory Id
+int   *mp_shared_mem_ptr;
 
 int main(){
+	int  rtn;
 	char c;
 	int shmid;
 	key_t key;
-	char *shm, *s;
+	
 
-	key = 5678;
+	key = 5678; 
 
 	// Create the segment.
-	if ((shmid = shmget(key, 5*sizeof(char), IPC_CREAT | 0666)) < 0) {
+	if ((shmid = shmget(key, sizeof(shm_content), IPC_CREAT | 0666)) < 0) {
 		perror("shmget");
 		exit(1);
 	}
 
 	// Now we attach the segment to our data space.
-	if ((shm = shmat(shmid, NULL, 0)) == (char*)-1) {
-		perror("shmat");
-		exit(1);
-	}
+	struct shm_content *pcontent = (struct shm_content*) shmat(shmid, 0, 0);
 
-	strncpy(shm, "Hola1", 5);
+	mptr = &(pcontent->mutex);
+
+    // Setup Mutex
+    if (rtn = pthread_mutexattr_init(&matr))
+    {
+        fprintf(stderr,"pthreas_mutexattr_init: %s",strerror(rtn)),exit(1);
+    }
+    if (rtn = pthread_mutexattr_setpshared(&matr,PTHREAD_PROCESS_SHARED))
+    {
+        fprintf(stderr,"pthread_mutexattr_setpshared %s",strerror(rtn)),exit(1);
+    }
+    if (rtn = pthread_mutex_init(mptr, &matr))
+    {
+        fprintf(stderr,"pthread_mutex_init %s",strerror(rtn)), exit(1);
+    }
+
+        // Lock mutex and then wait for signal to relase mutex
+    printf("child mutex lock \n");
+    pthread_mutex_lock( mptr );
+    printf("child mutex locked\n");
+
+    int i = 0; // :)
+
+    //busy wait
+    while (i<3)
+    {
+        printf("Busy Wait!!! I AM PROCESS 1\n");
+        //in the second process will change this line to :
+        //printf("Busy Wait!!! I AM PROCESS 2\n");
+        sleep(2);
+        i++;
+    }
+
+    printf("child mutex unlock\n");
+    pthread_mutex_unlock( mptr );
+    printf("child mutex unlocked\n");
 }
--- prueba2.c
+++ prueba2.c
@@ -4,7 +4,18 @@
 #include <string.h>
 #include <stdio.h> 
 #include <unistd.h>
+#include <pthread.h> 
 
+struct shm_content
+{
+      pthread_mutex_t   mutex;
+};
+
+pthread_mutex_t    *mptr; //Mutex Pointer
+pthread_mutexattr_t matr; //Mutex Attribute
+
+int   shared_mem_id;      //shared memory Id
+int   *mp_shared_mem_ptr; //shared memory ptr -- pointing to mutex
 
 int main(){
 	int shmid;
@@ -19,10 +30,28 @@ int main(){
 		exit(1);
 	}
 
-	//  Now we attach the segment to our data space.
-	if ((shm = shmat(shmid, NULL, 0)) == (char*) -1) {
-		perror("shmat");
-		exit(1);
-	}
-	printf("%s",shm);
+	struct shm_content *pcontent = (struct shm_content*) shmat(shmid, 0, 0);
+
+	mptr = &(pcontent->mutex);
+
+        // Lock mutex and then wait for signal to relase mutex
+    printf("child mutex lock \n");
+    pthread_mutex_lock( mptr );
+    printf("child mutex locked\n");
+
+    int i = 0; // :)
+
+    //busy wait
+    while (i<3)
+    {
+        printf("Busy Wait!!! I AM PROCESS 2\n");
+        //in the second process will change this line to :
+        //printf("Busy Wait!!! I AM PROCESS 2\n");
+        sleep(2);
+        i++;
+    }
+
+    printf("child mutex unlock\n");
+    pthread_mutex_unlock( mptr );
+    printf("child mutex unlocked\n");
 }
